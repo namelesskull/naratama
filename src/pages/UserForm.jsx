a4cc4845b7ec4ae5f3c7b32e9c8894f7cdb390b0
namelesskull <khaqulhudaa@gmail.com>
2025-05-19 17:46:12 +0700

    update form

--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -250,7 +250,7 @@ export const UserForm = () => {
               htmlFor='how'
               className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black peer-focus:dark:text-black-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
             >
-              Package
+              How you know us?
             </label>
           </div>
 
